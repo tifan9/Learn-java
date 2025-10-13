896c0f6f9c3abbe74fcc7f18888d72297fc77a87
uwase sophie <uwasesophie91@gmail.com>
2025-10-13 15:51:09 +0200

    Java course starting

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    public static void main (String a[]){
+        int num = 1;
+        System.out.println(num);
+        System.out.println("Hello this is a java files");
+
+        // Literals 
+        double num2 = 56;
+        System.out.println(num2);
+    }
+}

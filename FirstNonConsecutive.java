f63a90c4c370677b69c7de3e2af3a437176298d2
uwase sophie <uwasesophie91@gmail.com>
2025-10-20 10:27:53 +0200

    Java course starting

---
+++ FirstNonConsecutive.java
@@ -0,0 +1,10 @@
+public class FirstNonConsecutive {
+    static Integer find(final int[] array) {
+      for (int i = 1; i < array.length; i++ ){
+          if(array[i] != array[i-1] + 1){
+            return array[i];
+          }
+      }
+        return null;
+    }
+}

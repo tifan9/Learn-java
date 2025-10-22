02b30d8e25bb37a0e3ba46256bff4139412f2278
uwase sophie <uwasesophie91@gmail.com>
2025-10-22 18:30:45 +0200

    Java course heap and stacks

---
+++ codewars/Needle.java
@@ -0,0 +1,13 @@
+package codewars;
+
+public class Needle {
+      public static String findNeedle(Object[] haystack) {
+        for(int i = 0; i < haystack.length; i++){
+          if(haystack[i] == "needle"){
+            return "found the needle at position " + i;
+          }
+        }
+        return "";
+      }
+
+}
